0def82f0b08f298dc212b04b29fedadbce2cff02
Nate <minecraftcrusher100@gmail.com>
2022-12-13 17:24:16 +0000

    Add readthedocs config file

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,10 +19,9 @@ extensions = []
 templates_path = ['_templates']
 exclude_patterns = []
 
-
-
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+html_css_files = ["css/custom.css"]
